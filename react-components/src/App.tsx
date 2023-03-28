e6b28fa9bfa60269114e2dbe0a589e386faed89b
João Oliveira <jvcbatist4@outlook.com>
2023-03-28 14:12:55 -0300

    Eventos no JSX

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,17 +1,13 @@
-import Expressoes from "./components/Expressoes";
-import Header from "./components/Header";
-import Imagem from "./components/Imagem";
-
 function App() {
+
+  const handleShowMessage = () => {
+    alert('O botão foi clicado')
+  }
+
+
   return (
     <div>
-      <Header titleHeader="React Components"/>
-      Olá, sou um componente! (APP)
-      <Expressoes/>
-      {/* <Imagem url="https://www.google.com.br/google.jpg" subtitle="Logo marca do google"/> */}
-      <Imagem subtitle="Logo marca do google">
-        <img src="https://www.google.com.br/google.jpg" alt="" />
-      </Imagem>
+      <button onClick={handleShowMessage}>Clique aqui</button>
     </div>
   )
 }
