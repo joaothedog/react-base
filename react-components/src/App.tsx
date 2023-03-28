8e686cb2ef71cefabf29f02b4e896a62471b6769
João Oliveira <jvcbatist4@outlook.com>
2023-03-28 14:56:59 -0300

    Exercício de formulário

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 function App() {
   const [nome, setNome] = useState('');
+  const [sobrenome, setSobrenome] = useState('');
+  const [idade, setIdade] = useState(0);
 
   // const handleInput = (e: React.FormEvent<HTMLInputElement>) => {
   //   setNome(e.target.value);
@@ -10,11 +12,17 @@ function App() {
   return (
     <div>
       Nome:
-      <input 
-        value={nome} 
-        onChange={e => {setNome(e.target.value)}} 
-        type='text' />
-      <p>Meu nome é: {nome}</p>
+      <input type='text' value={nome} onChange={(e) => setNome(e.target.value)}/>
+      <br/>
+      Sobrenome:
+      <input type='text' value={sobrenome} onChange={(e) => setSobrenome(e.target.value)}/>
+      <br/>
+      Idade:
+      <input type='text' value={idade} onChange={(e) => setIdade(parseInt(e.target.value))}/>
+      <hr/>
+      Olá {nome} {sobrenome}, tudo bem?
+      <br/>
+      Você tem {idade} anos.
       
     </div>
   )
