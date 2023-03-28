d2ef4a643d9280c2e97ab5f25d17c087e0c11f6e
João Oliveira <jvcbatist4@outlook.com>
2023-03-28 14:27:50 -0300

    Utilização de states no React

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,13 +1,27 @@
-function App() {
+import { useState } from "react";
 
-  const handleShowMessage = () => {
-    alert('O botão foi clicado')
+function App() {
+  const [num, setNum] = useState(0);
+  const [name, setName] = useState('Carlos');
+
+  const handleCountNum = () => {
+    setNum(num+1);
   }
 
+  const handleChangeName = () => {
+    if(name === 'Carlos') {
+      setName('João');
+    } else {
+      setName('Carlos');
+    }
+  }
 
   return (
     <div>
-      <button onClick={handleShowMessage}>Clique aqui</button>
+      <p>O valor atual do número é: {num}</p>
+      <p>O meu nome é: {name}</p>
+      <button onClick={handleCountNum}>Clique aqui</button>
+      <button onClick={handleChangeName}>Clique para aparecer meu nome</button>
     </div>
   )
 }
