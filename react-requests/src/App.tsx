3014f4eab4ef432f62fcbe80fa86616cc367aeb4
João Oliveira <jvcbatist4@outlook.com>
2023-03-30 16:19:45 -0300

    Lidando com erros de requisição

--- react-requests/src/App.tsx
+++ react-requests/src/App.tsx
@@ -4,26 +4,42 @@ import { Movie } from "./types/Movie";
 
 const App = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+
+  const getMoviesTwo = () => {
+    fetch('https://api.b7web.com.br/cinema/').then((res) => {
+      return res.json();
+    }).then((json) => {
+      setMovies(json);
+    })
+  }
 
 
   const getMovies = async () => {
-    let res = await fetch('https://api.b7web.com.br/cinema/');
-    let json = await res.json();
-    setMovies(json);
-    setLoading(false);
+    try {
+      setLoading(true)
+      let res = await fetch('https://api.b7web.com.br/cinema/');
+      let json = await res.json();
+      setMovies(json);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      alert('Erro, tente novamente!')
+      console.log(error);
+    }
+    
   }
 
   useEffect(() => {
     getMovies();
-  }, [movies])
+  }, [])
 
 
   return(
     <div>
       <hr/>
 
-      Total de filmes: {loading ? 'Carregando total de filmes...' : movies.length}
+      Total de filmes: {loading && movies.length > 0 ? 'Carregando total de filmes...' : movies.length}
       {!loading &&
         <div style={{ display: 'flex', flexDirection: 'row'}}>
           {movies.map((item, key) => (
