2345743f4abba57b633bf852ff6c51dcc6a14737
João Oliveira <jvcbatist4@outlook.com>
2023-03-30 15:39:16 -0300

    Loading implementado

--- react-requests/src/App.tsx
+++ react-requests/src/App.tsx
@@ -4,42 +4,34 @@ import { Movie } from "./types/Movie";
 
 const App = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState(true);
 
-  // const getMovies = () => {
-  //   fetch('https://api.b7web.com.br/cinema/')
-  //     .then((res) => {
-  //       return res.json();
-  //     })
-  //     .then((json)=>{
-  //       setMovies(json);
-  //     })
-  // }
 
   const getMovies = async () => {
     let res = await fetch('https://api.b7web.com.br/cinema/');
     let json = await res.json();
     setMovies(json);
+    setLoading(false);
   }
 
   useEffect(() => {
-    // getMovies();
+    getMovies();
   }, [movies])
 
-  const handleLoadMovies = () => {
-    getMovies();
-  }
 
   return(
     <div>
-      <button onClick={handleLoadMovies}>Carregar filmes</button>
       <hr/>
 
-      Total de filmes: {movies.length}
-      <div style={{ display: 'flex', flexDirection: 'row'}}>
-        {movies.map((item, key) => (
-          <Filmes data={item} key={key}/>
-        ))}
-      </div>
+      Total de filmes: {loading ? 'Carregando total de filmes...' : movies.length}
+      {!loading &&
+        <div style={{ display: 'flex', flexDirection: 'row'}}>
+          {movies.map((item, key) => (
+            <Filmes data={item} key={key}/>
+          ))}
+        </div>
+      }
+      
     </div>
   );
 }
